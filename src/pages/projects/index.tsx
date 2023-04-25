7db24b3966b0d9ad673cfc641a317f817f179c64
Katleho Janco <katlehomabala3@gmail.com>
2023-04-25 21:29:50 +0200

    Added dummy content on table in projects

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -15,8 +15,8 @@ export default function Projects() {
           <section className="my-4">
             <InputField className="searchInput input" name="search" type="search" placeholder="Search here" />
           </section>
-          <section>
-            <table className="w-100 mt-4 border-collapse">
+          <section className="w-full overflow-x-auto">
+            <table className="mt-4 border-collapse table-auto">
               <thead className="bg-gray-100">
                 <tr>
                   <th>Units</th>
@@ -35,13 +35,22 @@ export default function Projects() {
                   <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
                   <td>Malcolm Lockyer</td>
                   <td>1961</td>
+                  <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                  <td>Malcolm Lockyer</td>
+                  <td>1961</td>
                 </tr>
                 <tr>
                   <td>Witchy Woman</td>
                   <td>The Eagles</td>
                   <td>1972</td>
+                  <td>Witchy Woman</td>
+                  <td>The Eagles</td>
+                  <td>1972</td>
                 </tr>
                 <tr>
+                  <td>Shining Star</td>
+                  <td>Earth, Wind, and Fire</td>
+                  <td>1975</td>
                   <td>Shining Star</td>
                   <td>Earth, Wind, and Fire</td>
                   <td>1975</td>
