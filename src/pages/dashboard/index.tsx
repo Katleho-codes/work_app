ccf92f0166d5f14cec58da7070e397a14b89ec2a
Katleho Janco <katlehomabala3@gmail.com>
2023-04-25 21:38:57 +0200

    Added dashboard screen

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <>
+            <main>
+                <div className="container">
+                    <h1>Dashboard</h1>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Dashboard
