c063bd33a8b25e874d3883652cbbf29ec2178363
Katleho Janco <katlehomabala3@gmail.com>
2023-04-26 22:46:45 +0200

    feat - added Navbar to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../../../components/Navbar'
 
 function Dashboard() {
     return (
         <>
+            <Navbar />
             <main>
                 <div className="container">
                     <h1>Dashboard</h1>
