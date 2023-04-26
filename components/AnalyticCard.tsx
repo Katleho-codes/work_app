a9eccc0582b3428589176e7433b971ddc1f9d867
Katleho Janco <katlehomabala3@gmail.com>
2023-04-26 23:09:25 +0200

    feat - added analytic card

---
+++ components/AnalyticCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AnalyticCard() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default AnalyticCard
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,7 +7,21 @@ function Dashboard() {
             <Navbar />
             <main>
                 <div className="container">
-                    <h1>Dashboard</h1>
+          <section>
+                      <h1>Dashboard</h1>
+                      <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"/></svg>
+                        Last month
+                      </button>
+          </section>
+
+                    <section>
+                        <article>
+                            <div className="dashboard_card_title">
+                                <h3>Units in</h3>
+                            </div>
+                        </article>
+                    </section>
                 </div>
             </main>
         </>
